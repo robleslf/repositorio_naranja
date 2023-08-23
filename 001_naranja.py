be6600fe7844c9019ec75edede55d145023bb2d1
robleslf <robleslf93@gmail.com>
2023-08-23 18:33:39 +0200

    Añadida la librería de separadores a "Naranja"

--- 001_naranja.py
+++ 001_naranja.py
@@ -1,7 +1,13 @@
+from lib_separadores import *
+
+separador_titulo("Naranja 1.0")
+
 while True:
     color = input("Introduce un color: ")
+    separador_1()
     if color.lower().strip() == "naranja":
         print("Me parece una buena elección, aunque cualquier decisión tuya me habría parecido bien.")
+        print("(=⌒‿‿⌒=)")
         break
     else:
         print("¿No sería mejor elegir naranja?")
